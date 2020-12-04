19dd3fd508c63adee79b9be423d48d0f85714ec1
itachips@yahoo.co.jp <itachips@yahoo.co.jp>
2020-12-04 16:34:48 +0900

    初期バージョン

---
+++ Sushida/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Sushida
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "SushI/O";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Ctrl_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Sushida/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Media.Ocr;
+using Windows.Graphics.Imaging;
+using Tesseract;
+
+namespace Sushida
+{
+    public partial class Form1 : Form
+    {
+        bool isStarted = false;//実行中か
+        bool isAutoMiss = false;//あえてミスする
+
+        private Point mousePoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (isStarted)
+            {
+                this.Text = this.Text + " - F1で停止";
+            }
+            else
+            {
+                this.Text = this.Text + " - F1で開始";
+            }
+        }
+
+        public string GetOCR(Bitmap img)
+        {
+            var tesseract = new Tesseract.TesseractEngine("TrainData", "eng");
+            Tesseract.Page page = tesseract.Process(img);
+
+            Text = page.GetText();
+            return page.GetText();
+        }
+
+        public Bitmap GetScr()
+        {
+            
+            Bitmap ScrBmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width,Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(ScrBmp);
+            g.CopyFromScreen(0, 0, 0, 0, ScrBmp.Size);
+            g.Dispose();
+
+            Bitmap WinShot = ScrBmp.Clone(new Rectangle(PointToScreen(new Point(0,0)).X, PointToScreen(new Point(0, 0)).Y,
+                ClientSize.Width, ClientSize.Height),ScrBmp.PixelFormat);
+
+            ScrBmp.Dispose();
+
+            //WinShot.Save("SS.png", System.Drawing.Imaging.ImageFormat.Png);
+            return WinShot;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string str = GetOCR(GetScr());
+
+            if (isStarted)
+            {
+                this.Text = this.Text + " - F1で停止";
+            }
+            else
+            {
+                this.Text = this.Text + " - F1で開始";
+            }
+
+            //大文字だったら誤認識判定
+            bool Err = false;
+            foreach(char c in str)
+            {
+                if (char.IsUpper(c))
+                    Err = true;
+            }
+
+            try
+            {
+                if (Err == false)
+                {
+                    if (isAutoMiss)
+                    {
+                        SendKeys.Send("a"+str+"a");
+                    }
+                    else
+                    {
+                        SendKeys.Send(str);
+                    }
+                }
+
+            }
+            catch { }
+
+
+        }
+
+        private void Ctrl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1)
+            {
+                isStarted = !isStarted;
+
+                if (isStarted)
+                {
+                    timer1.Enabled = true;
+                    timer1_Tick(null,null);
+                }
+                else
+                {
+                    timer1.Enabled = false;
+                }
+
+
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                this.Left += e.X - mousePoint.X;
+                this.Top += e.Y - mousePoint.Y;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                mousePoint = new Point(e.X, e.Y);
+            }
+        }
+    }
+}
